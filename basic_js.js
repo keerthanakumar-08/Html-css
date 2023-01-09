f72c6b48122e130f05c96d53cf6fd35654afebfe
keerthana <keerthana.v@dorustree.in>
2023-01-09 20:06:40 +0530

    js fundamentals

---
+++ basic_js.js
@@ -0,0 +1,43 @@
+// alert("Welcome to javascript");
+console.log(1);
+
+
+let age = 19;
+console.log(age)
+
+let year = 2000;
+console.log(year)
+
+age=33;
+console.log(age)
+
+// const point = 10;
+// point = 1;
+
+
+
+let emailId = 'keerthanavijayakumar@gmail.com';
+
+console.log(emailId)
+
+let result = emailId[8]
+
+console.log(result)
+
+let resultupper = emailId.toUpperCase();
+
+console.log(resultupper)
+
+console.log(resultupper.toLowerCase());
+
+console.log(resultupper.length);
+
+console.log(resultupper.indexOf('n'));
+
+console.log(resultupper.lastIndexOf('r'));
+
+console.log(resultupper.slice(2,5));
+
+console.log(resultupper.substring(0,10));
+
+console.log(resultupper.replace('E','R'));
